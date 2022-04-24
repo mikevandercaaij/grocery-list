2630f5b8345b3ce4b731fb50a02f8bf2d477ab72
Mike van der Caaij <mvdc2000@hotmail.nl>
2022-04-24 16:22:30 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const date = require(__dirname + "/date.js");
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000;
+
+let items = [];
+let workItems = [];
+
+app.route("/")
+    .get((req, res) => {
+        res.render("list", { listTitle: date.getDate(), listItems: items });
+    })
+    .post((req, res) => {
+        const item = req.body.newItem;
+        if (req.body.list === "Work") {
+            workItems.push(item);
+            res.redirect("/work");
+        } else {
+            items.push(item);
+            res.redirect("/");
+        }
+    });
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", listItems: workItems });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
